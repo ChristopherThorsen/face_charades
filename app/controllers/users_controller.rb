0dace7f7eda34d97a2989cba6c6952130efd09b0
ChristopherThorsen <Thorsen41@gmail.com>
2014-08-05 15:18:19 -0400

    users are assigned their facebook names as names

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,12 @@ class UsersController < ApplicationController
     graph = Koala::Facebook::API.new(session[:access_token])
     user = graph.get_object("me")
     facebook_id = user["id"]
-    if User.find_by(facebook_id: facebook_id)
+    name = user["first_name"] + " " + user["last_name"]
+    user = User.find_by(facebook_id: facebook_id)
+    if user
+      user.update(name: name)
     else
-      User.create(facebook_id: facebook_id, name: facebook_id)
+      User.create(facebook_id: facebook_id, name: name)
     end
 
     redirect_to dashboard_path
--- db/migrate/20140805184337_add_name_column_to_users.rb
+++ db/migrate/20140805184337_add_name_column_to_users.rb
@@ -1,5 +1,5 @@
 class AddNameColumnToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :name, :string, null: false, uniqueness: true
+    add_column :users, :name, :string, null: false
   end
 end
