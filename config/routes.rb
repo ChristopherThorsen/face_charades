637c737c03792479e88d46eb0f1499722bd57b88
ChristopherThorsen <Thorsen41@gmail.com>
2014-08-06 11:53:13 -0400

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: "sessions#new"
   resource :dashboard, only: [:show]
   get "/sessions", to: "sessions#create"
-  get "/users", to: "users#create"
+  post "/users", to: "users#create"
 end
