02a05ce2f2629a77299f2762d658eaf7e90ae96f
ChristopherThorsen <Thorsen41@gmail.com>
2014-08-18 13:38:54 -0400

    users see best guesser leaderboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,6 +4,14 @@ class DashboardsController < ApplicationController
     @guesses = current_user.guesses
     @pending_guesses = current_user.pending_guesses.sort_by_time
     @card = Card.pick_random_card
+    @leaders = best_guesser_leaderboard
+  end
+
+  private
+
+  def best_guesser_leaderboard
+    leader_ids = current_user.friend_ids + [current_user.id]
+    User.where(id: [leader_ids]).order("total_received_games_won DESC").limit(7)
   end
 
 end
